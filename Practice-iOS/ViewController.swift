ccd7f6ded318ceb0923004385a91dce39757a085
Simon Yang <simonyang81@gmail.com>
2015-08-28 16:34:27 +0800

    Label

--- Practice-iOS/ViewController.swift
+++ Practice-iOS/ViewController.swift
@@ -7,13 +7,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+
+
+    let label1 = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        view.addSubview(label1)
+        
+        label1.textColor = UIColor.blackColor()
+        label1.font = UIFont(name: "HelveticaNeue", size: 30.0)
+        label1.text = "Hello World"
+        label1.textAlignment = NSTextAlignment.Center
+        
+        label1.snp_remakeConstraints { (make) -> Void in
+            make.width.equalTo(view)
+            make.center.equalTo(view)
+        }
+        
+//        label1.re
+
     }
+
+//    func
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
